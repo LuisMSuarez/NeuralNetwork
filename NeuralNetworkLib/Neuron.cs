75b067119d6b9b782fabed9648b23d757593d333
LuisMSuarez <140195810+LuisMSuarez@users.noreply.github.com>
2024-11-04 23:16:31 -0800

    core logic

--- NeuralNetworkLib/Neuron.cs
+++ NeuralNetworkLib/Neuron.cs
@@ -4,7 +4,17 @@
     {
         private int bias;
         private int value;
-        private 
+        private readonly IEnumerable<Synapse> successors;
+        private readonly IEnumerable<Neuron> predecessors;
+        private readonly IEnumerable<int> values;
+
+        public Neuron()
+        {
+            successors = new List<Synapse>();
+            predecessors = new List<Neuron>();
+            this.values = new List<int>();
+            bias = 0;
+        }
 
         /// <summary>
         /// Used to set a value when the neuron belongs to the input layer
@@ -12,13 +22,40 @@
         /// <param name="value"></param>
         public void SetValue(int value)
         {
-            throw new NotImplementedException();
+            this.value = value;
         }
 
         public void ConnectToSuccessor(Neuron successor, int weight)
-        {  throw new NotImplementedException(); }
+        {
+            var synapse = new Synapse(successor, weight);
+            successors.Append(synapse);
+            successor.ConnectToPredecessor(this);
+        }
 
-        public void ConnectToPredecessor(Neuron predecessor) 
-        { throw new NotImplementedException();
+        public void ConnectToPredecessor(Neuron predecessor)
+        {
+            predecessors.Append(predecessor);
+        }
+
+        private void InputValue(int value)
+        {
+            this.values.Append(value);
+
+            // If we have recieved all inputs from all predecessors, we can calculate the value for this neuron
+            if (this.values.Count() == this.predecessors.Count())
+            {
+                this.value = this.values.Aggregate((item, accumulated) => accumulated + item) + this.bias;
+                this.InvokeNextLayer();
+            }
+        }
+
+        public void InvokeNextLayer()
+        {
+            foreach (var synapse in successors)
+            {
+                var synapseValue = this.value * synapse.Weight;
+                synapse.Neuron.InputValue(synapseValue);
+            }
+        }
     }
 }
--- NeuralNetworkLib/Synapse.cs
+++ NeuralNetworkLib/Synapse.cs
@@ -8,9 +8,16 @@ namespace NeuralNetworkLib
 {
     internal class Synapse
     {
+        public int Weight { get; private set; }
+
+        public Neuron Neuron { get; private set; }
+
+        public int MyProperty { get; private set; }
+
         public Synapse(Neuron neuron, int weight) 
         {
-            throw new NotImplementedException();
+            this.Neuron = neuron;
+            this.Weight = weight;
         }
     }
 }
